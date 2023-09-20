e22a577fdc9b0916800fdcee7257b92af4f3924d
SArie_ <lethaldrum97@gmail.com>
2023-09-20 19:25:22 +0530

    Added HTTP Requests to API View

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,14 +1,21 @@
 from rest_framework.views import APIView
-from rest_framework.response import  Response
+from rest_framework.response import Response
+
+from rest_framework import status  # Handy HTTP Status codes
+from profile_api import serializers  #
 
 
 class HelloAPIView(APIView):
     """ Test API View """
+    # serializer_class
+    serializer_class = serializers.HelloSerializer
+
+    # //////////////////////////////////////////////////////////////////////////
     def get(self, request, format=None):
 
         """ Returns a list of APIView Features """
 
-        an_apiview =[
+        an_apiview = [
             'uses HTTP method function (get, post, patch, put, delete)',
             'is similar to traditional DJANGO view',
             'Gives most control over app logic',
@@ -16,5 +23,46 @@ class HelloAPIView(APIView):
         ]
 
         # Definining APi with get function
-        return  Response({'message': "Hello",
-                          'an_apiview': an_apiview })
+        return Response({'message': "Hello",
+                         'an_apiview': an_apiview})
+
+    # //////////////////////////////////////////////////////////////////////////
+
+    def post(self, request):
+        """ Create a hello message with our name """
+
+        # self.serializer is a standard way to retrieve serializer value
+        serializer = self.serializer_class(data=request.data)
+
+        # Checking
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello! {name}'
+
+            return Response({'message': message})
+
+        else:
+            # Returns HTTP 400 bad request error
+
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    # //////////////////////////////////////////////////////////////////////////
+
+    def put(self,request,pk=None):
+        """ Handles Updating an Object """
+        return  Response ({'method':'PUT'})
+
+    # //////////////////////////////////////////////////////////////////////////
+
+    def patch(self,request,pk=None):
+        """ Handles a partial update of an object """
+        return  Response({'method':'PATCH'})
+
+    # //////////////////////////////////////////////////////////////////////////
+
+    def delete(self,request,pk=None):
+        """ Responsible for Deleting values """
+        return  Response ({'method':'DELETE'})
